445a043f66abd489818d34630a5657e6124b2570
AnthonyA <alicea.9a@gmail.com>
2024-10-24 11:12:48 -0600

    Refactored to handle empty arrays and single element arrays. Added more thorough test cases to ensure quality

--- tail.js
+++ tail.js
@@ -1,19 +1,32 @@
 const assertEqual = function(actual,expected) {
-  return console.log(actual === expected ? `🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`
-    : `🟥🟥🟥 Assertion Failed: [${actual}] !== [${expected}]`);
+  return actual === expected ? `🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`
+    : `🟥🟥🟥 Assertion Failed: [${actual}] !== [${expected}]`;
 };
 
 const tail = function(array) {
   let tail = array.map((x) => x);
+  if(tail.length === 0){
+    return array;
+  }
   tail.shift();
   return tail;
 };
-const temp1 = [1,2,3];
+const test1 = [1,2,3];
 const temp2 = ['hi','bye','sigh'];
+const test3 = [];
+const test4 = [1];
 
-console.log(assertEqual(tail(temp1)[0],2));
-console.log(assertEqual(tail(temp1)[1],3));
-console.log(temp1);
+console.log(assertEqual(tail(test1)[0],2));
+console.log(assertEqual(tail(test1)[1],3));
+console.log(assertEqual(tail(test1).length,test1.length -1));
+console.log(test1);
+console.log('--1--');
 console.log(assertEqual(tail(temp2)[0],'bye'));
 console.log(assertEqual(tail(temp2)[1],'sigh'));
+console.log(assertEqual(tail(temp2).length,temp2.length -1));
 console.log(temp2);
+console.log('--2--');
+console.log(assertEqual(tail(test3).length,0));
+console.log('--3--');
+console.log(assertEqual(tail(test4).length,0));
+console.log('--4--');
