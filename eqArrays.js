8ba354636cbc6e49260aace5d432e5991f6fee59
AnthonyA <alicea.9a@gmail.com>
2024-10-26 11:38:09 -0600

    Added eqArray function that returns if two arrays are equal or not

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual,expected) {
+  return console.log(actual === expected ? `🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`
+    : `🟥🟥🟥 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+const eqArrays = function(array1,array2){
+  if(array1.length !== array2.length){
+    return false;
+  }
+  for(let ele in array1){
+    if(array1[ele] != array2[ele]){
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1,2,3],[1,2,3]),true);
+assertEqual(eqArrays(['hi','bye','sigh'],['hi','bye','sigh']),true);
+assertEqual(eqArrays([4,1,2,3],[1,2,4]),false);
+assertEqual(eqArrays([1,2,3],[1,2,2]),false);
+assertEqual(eqArrays(['hi','bye','sigh'],['hi','bye','my']),false);
+assertEqual(eqArrays([],[]),true);
+assertEqual(eqArrays([1],[1]),true);
