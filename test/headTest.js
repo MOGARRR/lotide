5bc380faa5dce57364edbbb77bc5ff3647862dfc
AnthonyA <alicea.9a@gmail.com>
2024-11-13 12:28:30 -0700

    Refactored headTest - remade more descriptive test with mocha/chai - added gitignore file

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,23 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
-assertEqual(head([1,2,3,4,5]),1);
-assertEqual(head(['hi','bye','hey']),'hi');
-assertEqual(head([true,false]),true);
-assertEqual(head([]),undefined);
-assertEqual(head([1]),1);
+
+describe('#head', () => {
+  it('returns 1 for [1,2,3]', () => {
+    assert.strictEqual(head([1,2,3]),1);
+  });
+  it('returns "5" for ["5"]', () =>{
+    assert.strictEqual(head(['5']),'5');
+  });
+  it("returns 'hi' for ['hi','bye','hey']", () =>{
+    assert.strictEqual(head(['hi','bye','hey']),'hi');
+  });
+  it("returns true for [true,false]", () =>{
+    assert.strictEqual(head([true,false]),true);
+  });
+  it("returns 1 for [1]", () =>{
+    assert.strictEqual(head([1]),1);
+  });
+  it("returns undefined for empty array", () =>{
+    assert.strictEqual(head([]),undefined);
+  });
+});
