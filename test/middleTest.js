2d1ea06d16003d70b6737c5a14ffbeefea027bdf
AnthonyA <alicea.9a@gmail.com>
2024-11-13 21:03:40 -0700

    Refactored middleTest.js - rewrote the test cases with mocha for better quality assurance

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
-assertArraysEqual(middle([1,2,3,4,5,6,7]),[4]);
-assertArraysEqual(middle([1,2]),[]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([]),[]);
+describe('#middle', () => {
+  it('returns the 2 middle indexes [3,4] of an even array [1,2,3,4,5,6]', () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]),[3,4]);
+  });
+  it('returns the middle index [4] of an odd array [1,2,3,4,5,6,7]', () => {
+    assert.deepEqual(middle([1,2,3,4,5,6,7]),[4]);
+  });
+  it('returns [] if the length is 2', () => {
+    assert.deepEqual(middle([1,2]),[]);
+  });
+  it('returns an [] if array has 1 value', () => {
+    assert.deepEqual(middle([1]),[]);
+  });
+  it('returns [] if array has no value ', () => {
+    assert.deepEqual(middle([]),[]);
+  });
+});
+
+
