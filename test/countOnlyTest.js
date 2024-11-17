1bff4ae4e293a7e0da32f45342159267c08fcff2
AnthonyA <alicea.9a@gmail.com>
2024-11-16 20:23:54 -0700

    Test cases added and passed for: - countOnly.js - findKeyByValue.js

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,7 +1,32 @@
 const countOnly = require('../countOnly');
 const assert = require('chai').assert;
-describe('#function', () => {
-  it('' , () => {
-    
+describe('#countOnly', () => {
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe",
+  ];
+  const result1 = countOnly(firstNames, {
+    Jason: true,
+    Karima: true,
+    Fang: true,
+    Agouhanna: false,
+  });
+
+  it('Returns 1 for when item occurs once' , () => {
+    assert.strictEqual(result1["Jason"],1);
+  });
+
+  it('Returns 2 when item appears twice' , () => {
+    assert.strictEqual(result1["Fang"],2);
+  });
+  it('Returns undefined when item is false' , () => {
+    assert.strictEqual(result1["Agouhanna"],undefined);
   });
 });
--- test/findKeyByValueTest.js
+++ test/findKeyByValueTest.js
@@ -1,7 +1,23 @@
 const findKeyByValue = require('../findKeyByValue');
 const assert = require('chai').assert;
-describe('#function', () => {
-  it('' , () => {
-    
+describe('#findKeyByValue', () => {
+  it('Returns key when given string value' , () => {
+    assert.strictEqual(findKeyByValue({drama: "The Wire", comedy: "Brooklyn Nine-Nine", sci_fi: "The Expanse"},"The Wire"), "drama");
+  });
+
+  it('Returns undefined if object does not contain value' , () => {
+    assert.strictEqual(findKeyByValue({drama: "The Wire", comedy: "Brooklyn Nine-Nine", sci_fi: "The Expanse"},"That 70s Show"), undefined);
+  });
+
+  it('Returns key when given number value' , () => {
+    assert.strictEqual(findKeyByValue({apples: 3, oranges: 5, bananas: 43}, 43), "bananas");
+  });
+
+  it('Returns key when given boolean value' , () => {
+    assert.strictEqual(findKeyByValue({ green: false, orange: true, red: false}, true), "orange");
+  });
+
+  it('Returns first key that matches value' , () => {
+    assert.strictEqual(findKeyByValue({ green: false, orange: true, red: false, yellow: true}, true), "orange");
   });
 });
