997087cd0dc38c1c0c749a49761d64c2bffbdf2b
AnthonyA <alicea.9a@gmail.com>
2024-11-16 21:16:23 -0700

    Removed copy file.Added test cases and passed for: - eqArray.js - eqObjects.js

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,7 +1,32 @@
 const eqArrays = require('../eqArrays');
 const assert = require('chai').assert;
-describe('#function', () => {
-  it('' , () => {
-    
+describe('#eqArray', () => {
+  it('Returns true with matching arrays' , () => {
+    assert.deepEqual(eqArrays([1,2,3],[1,2,3]),true);
   });
+
+  it('Returns false with different arrays' , () => {
+    assert.deepEqual(eqArrays([1,2,3],[1,2,5]),false);
+  });
+
+  it('Works with booleans' , () => {
+    assert.deepEqual(eqArrays([true,false,false],[true,false,false]),true);
+  });
+
+  it('Works with strings' , () => {
+    assert.deepEqual(eqArrays(['hi','bye','sigh'],['hi','bye','sigh']),true);
+  });
+
+  it('Works with nested arrays' , () => {
+    assert.deepEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]),true);
+  });
+
+  it('Works with nested multiple value types' , () => {
+    assert.deepEqual(eqArrays([[2, 3], ['4',[false]]], [[2, 3], ['4',[false]]]),true);
+  });
+
+  it('Still returns false with multiple value types' , () => {
+    assert.deepEqual(eqArrays([[2, 3], [4,[false]]], [[2, 3], ['4',[false]]]),false);
+  });
+  
 });
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,7 +1,20 @@
 const eqObjects = require('../eqObjects');
 const assert = require('chai').assert;
-describe('#function', () => {
-  it('' , () => {
-    
+describe('#eqObjects', () => {
+  it('Returns true for matching objects' , () => {
+    assert.equal(eqObjects({a:1,b:2,c:3},{a:1,b:2,c:3}),true);
   });
+
+  it('Returns false for different objects' , () => {
+    assert.equal(eqObjects({a:1,b:2,c:3},{a:1,b:2,f:7}),false);
+  });
+
+  it('Works with multiple data types' , () => {
+    assert.equal(eqObjects({a:true,b:"apple",c:34,array:[1,2,3]},{a:true,b:"apple",c:34,array:[1,2,3]}),true);
+  });
+
+  it('Works with nested objects' , () => {
+    assert.equal(eqObjects({a:1,b:2,c:{realC:3}},{a:1,b:2,c:{realC:3}}),true);
+  });
+
 });
